a4916cd74d2b9973bbb06591e5060229e80f9a3f
ulfangor <inesanzil@gmail.com>
2026-01-01 19:43:32 +0100

    test branch ines

--- app/src/main/java/com/example/lolop/DetailChampion.java
+++ app/src/main/java/com/example/lolop/DetailChampion.java
@@ -3,5 +3,8 @@ package com.example.lolop;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailChampion extends AppCompatActivity {
+
     private final String currentVersion = "14.5.1";
+
+
 }
