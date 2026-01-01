5541a292231ef8afecf43bd46497f74ba6991761
ulfangor <inesanzil@gmail.com>
2026-01-01 19:43:04 +0100

    test bra

--- app/src/main/java/com/example/lolop/DetailChampion.java
+++ app/src/main/java/com/example/lolop/DetailChampion.java
@@ -3,4 +3,5 @@ package com.example.lolop;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailChampion extends AppCompatActivity {
+    private final String currentVersion = "14.5.1";
 }
