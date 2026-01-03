93d957cfce5ae7a929dfeba80ae5bc35f2f87830
ulfangor <inesanzil@gmail.com>
2026-01-03 17:44:53 +0100

    possibilité de retour à la liste des champions

--- app/src/main/java/com/example/lolop/DetailChampion.java
+++ app/src/main/java/com/example/lolop/DetailChampion.java
@@ -1,10 +1,13 @@
 package com.example.lolop;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
 import com.example.lolop.model.Champion;
@@ -18,6 +21,14 @@ public class DetailChampion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailchampion);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
         ImageView championSplash = findViewById(R.id.champion_splash);
         TextView championName = findViewById(R.id.champion_name);
         TextView championTitle = findViewById(R.id.champion_title);
@@ -26,6 +37,9 @@ public class DetailChampion extends AppCompatActivity {
         Champion champion = getIntent().getParcelableExtra(EXTRA_CHAMPION);
 
         if (champion != null) {
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().setTitle(champion.getName());
+            }
             championName.setText(champion.getName());
             championTitle.setText(champion.getTitle());
             championLore.setText(champion.getLore());
@@ -36,5 +50,14 @@ public class DetailChampion extends AppCompatActivity {
                     .load(splashUrl)
                     .into(championSplash);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
